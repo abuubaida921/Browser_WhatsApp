2f4cc8e3928192b2cfba1b131de039ef4ae7015c
Abu Ubaida <ubaidanazia@gmail.com>
2024-10-03 16:29:15 +0600

    New Modifications

--- lib/app/modules/home/controllers/home_screen_controller.dart
+++ lib/app/modules/home/controllers/home_screen_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
@@ -23,12 +24,71 @@ class HomeScreenController extends GetxController {
 
   double progress = 0;
   var currentURL = 'https://abuubaida921.com/';
-  bool isFirstTime = true;
-  late InAppWebViewController inAppWebViewController;
-  var isPreviousPage = false;
-  var isNextPage = false;
+  final urlController = TextEditingController();
+  final GlobalKey webViewKey = GlobalKey();
+  InAppWebViewController? webViewController;
+
+  InAppWebViewSettings settings = InAppWebViewSettings(
+      isInspectable: kDebugMode,
+      mediaPlaybackRequiresUserGesture: false,
+      allowsInlineMediaPlayback: true,
+      iframeAllow: "camera; microphone",
+      iframeAllowFullscreen: true);
+
+
+  late ContextMenu contextMenu;
   PullToRefreshController? pullToRefreshController;
-  PullToRefreshOptions pullToRefreshSettings = PullToRefreshOptions(
-    color: Colors.blue,
-  );
+
+  @override
+  void onInit() {
+    super.onInit();
+
+
+    contextMenu = ContextMenu(
+        menuItems: [
+          ContextMenuItem(
+              id: 1,
+              title: "Special",
+              action: () async {
+                print("Menu item Special clicked!");
+                print(await webViewController?.getSelectedText());
+                await webViewController?.clearFocus();
+              })
+        ],
+        settings: ContextMenuSettings(hideDefaultSystemContextMenuItems: false),
+        onCreateContextMenu: (hitTestResult) async {
+          print("onCreateContextMenu");
+          print(hitTestResult.extra);
+          print(await webViewController?.getSelectedText());
+        },
+        onHideContextMenu: () {
+          print("onHideContextMenu");
+        },
+        onContextMenuActionItemClicked: (contextMenuItemClicked) async {
+          var id = contextMenuItemClicked.id;
+          print("onContextMenuActionItemClicked: " +
+              id.toString() +
+              " " +
+              contextMenuItemClicked.title);
+        });
+
+    pullToRefreshController = kIsWeb ||
+        ![TargetPlatform.iOS, TargetPlatform.android]
+            .contains(defaultTargetPlatform)
+        ? null
+        : PullToRefreshController(
+      settings: PullToRefreshSettings(
+        color: Colors.blue,
+      ),
+      onRefresh: () async {
+        if (defaultTargetPlatform == TargetPlatform.android) {
+          webViewController?.reload();
+        } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+          webViewController?.loadUrl(
+              urlRequest:
+              URLRequest(url: await webViewController?.getUrl()));
+        }
+      },
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:get/get.dart';
 
 import 'app/global_binding.dart';
 import 'app/routes/app_pages.dart';
 
-
 late SharedPreferences prefs;
+
+WebViewEnvironment? webViewEnvironment;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.windows) {
+    final availableVersion = await WebViewEnvironment.getAvailableVersion();
+    assert(availableVersion != null,
+        'Failed to find an installed WebView2 runtime or non-stable Microsoft Edge installation.');
+
+    webViewEnvironment = await WebViewEnvironment.create(
+        settings: WebViewEnvironmentSettings(userDataFolder: 'custom_path'));
+  }
   prefs = await SharedPreferences.getInstance();
   runApp(const MyApp());
 }
@@ -22,7 +33,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
