ef06f09c718e633a03f6b8b0dadfc208fdfb0e58
Abu Ubaida <ubaidanazia@gmail.com>
2024-10-05 14:17:05 +0600

    Another approch

--- lib/controller.dart
+++ lib/controller.dart
@@ -86,4 +86,36 @@ class newBrowserController extends GetxController {
     if (listOfGlobalKey.isNotEmpty) selectedIndex.value = 0;
   }
 
+  final browserInstances = <BrowserInstance> [].obs;
+
+  // final selectedIndex = 0.obs;
+
+  void addBrowser() {
+    browserInstances.add(BrowserInstance());
+  }
+
+  void removeBrowser(int index) {
+    if (browserInstances.length > index) {
+      browserInstances.removeAt(index);
+      if (selectedIndex.value >= browserInstances.length) {
+        selectedIndex.value = browserInstances.length - 1;
+      }
+    }
+  }
+
+}
+
+
+class BrowserInstance {
+  final GlobalKey webViewKey = GlobalKey();
+  InAppWebViewController? webViewController;
+  final CookieManager cookieManager = CookieManager();
+
+  InAppWebViewSettings settings = InAppWebViewSettings(
+    isInspectable: true,
+    mediaPlaybackRequiresUserGesture: false,
+    allowsInlineMediaPlayback: true,
+    iframeAllow: "camera; microphone",
+    iframeAllowFullscreen: true,
+  );
 }
